f42f14874d11740ec1492acf8958228ec8810c4c
danielvinzia <danielvinzia@gmail.com>
2022-01-31 16:25:51 -0300

    agregado herencia en java y PHP

---
+++ CursoPOOUber/PHP/UberBlack.php
@@ -0,0 +1,12 @@
+<?php
+class UberBlack extends Car{
+    public $typeCarAccepted;
+    public $material;
+
+    public function __construct($license, Account $driver, $typeCarAccepted, $material)
+    {
+        parent::__construct($license,$driver); 
+        $this->typeCarAccepted = $typeCarAccepted;
+        $this->material = $material;
+    }
+}
---
+++ CursoPOOUber/PHP/UberVan.php
@@ -0,0 +1,12 @@
+<?php
+class UberVan extends Car{
+    public $typeCarAccepted;
+    public $material;
+
+    public function __construct($license, Account $driver, $typeCarAccepted, $material)
+    {
+        parent::__construct($license, $driver);
+        $this->typeCarAccepted = $typeCarAccepted;
+        $this->material = $material;
+    }
+}
