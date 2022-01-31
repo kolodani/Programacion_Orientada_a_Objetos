ed379c26868b44966cc1c83266c22e9201793e29
danielvinzia <danielvinzia@gmail.com>
2022-01-31 15:47:44 -0300

    terminado lo de php

--- CursoPOOUber/PHP/Car.php
+++ CursoPOOUber/PHP/Car.php
@@ -1,8 +1,21 @@
 <?php
+require_once 'Account.php';
     class Car {
         public $id;
         public $license;
         public $driver;
         public $passengers;
+        
+        public function __construct($license, Account $driver)
+        {
+            $this ->license = $license;
+            $this ->driver = $driver;
+        }
+        
+        public function PrintDataCar(){
+            echo "la licencia es: ".$this-> license. 
+            "el conductor es: " .$this->driver->name. 
+            "y su documento es:" .$this->driver->document;
+        }
     }
 ?>
