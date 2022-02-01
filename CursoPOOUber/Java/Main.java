f08276e1ddb2bbb76a9191d158c915f7247bb024
danielvinzia <danielvinzia@gmail.com>
2022-02-01 12:26:37 -0300

    clase de encapsulamiento

--- CursoPOOUber/Java/Main.java
+++ CursoPOOUber/Java/Main.java
@@ -2,12 +2,12 @@ class Main {
     public static void main(String[] args) {
         System.out.println("conductor uber");
         Car car = new Car ("AMQ123", new Account("Andres Herrera", "AND123","andresl@hotmail.com","123459"));
-        car.passenger = 4;
+        car.setPassenger(4);
         car.printDataCar();
         
         System.out.println("conductor UberX");
         UberX uberX = new UberX("MNB321", new Account("Ana ferran", "HGF852", "analita@hotmail.com", "987456"),"Toyota", "corolla");
-        uberX.passenger = 4;
+        uberX.setPassenger(4);
         uberX.printDataUberX();
         
         System.out.println("Usuario");
