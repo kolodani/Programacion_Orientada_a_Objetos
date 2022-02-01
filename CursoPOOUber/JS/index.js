e5c1875777dc05bc84e9c1e99deab7e0c24b9c46
danielvinzia <danielvinzia@gmail.com>
2022-02-01 10:35:50 -0300

    agregada todas las clases en JavaScript

--- CursoPOOUber/JS/index.js
+++ CursoPOOUber/JS/index.js
@@ -1,3 +1,7 @@
 var car = new Car("AWD456", new Account("Andres Herrera", "34.325.542"));
 car.passenger = 4;
 car.printDataCar();
+
+var uberX = new UberX("KJH743", new Account("Ana Ferran", "65.361.654"), "Chevrolet", "Spark");
+uberX.passenger = 4;
+uberX.printDataCar();
